f13736a4fe38668aaefc20b08e6e820508e070f0
Michelle Chen <mich.chen.94@gmail.com>
2020-10-04 12:13:24 -0700

    completed in javascript

---
+++ jumping_on_clouds.js
@@ -0,0 +1,16 @@
+function jumpingOnClouds(c) {
+    
+    let count = 0;
+    
+    for (let i = 0; i < (c.length - 1); i++) {
+        if (c[i] == 0) {
+            i++
+        }
+        count++
+    }
+
+    return count
+}
+
+console.log(jumpingOnClouds([0, 0, 1, 0, 0, 1, 0]))
+console.log(jumpingOnClouds([0, 0, 0, 1, 0, 0]))
